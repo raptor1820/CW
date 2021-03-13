0953d503758ca8f628d9a1c3d251db1dd0e00501
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2021-03-13 11:59:51 +0530

    13/3/2010 12:00

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const parser = require("body-parser");
 const app = express();
 const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.json());
+app.use(parser.json());
 app.listen(port, function (error) {
   if (error) {
     console.log(error);
@@ -15,23 +16,22 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 function validator(req, res, next) {
-  var object1 = JSON.parse(req);
-  console.log(object1);
-  var data = req.body.email;
-  var password = req.body.password;
+  // var data = req.body.email;
+  // var password = req.body.password;
 
   // var count = 0;
-  // if ((await data.search("@")) != -1) {
+  // if (data.search("@") != -1) {
   //   count++;
   // }
-  // if ((await data.search(".com")) != -1) {
+  // if (data.search(".com") != -1) {
   //   count++;
   // }
-  // if ((await count) == 2) {
+  // if (count == 2) {
   //   next();
+  // } else {
+  //   res.send("Incorrect email format");
   // }
-  console.log(typeof data);
-  res.send("Incorrect email format");
+  next();
 }
 app.post("/form", validator, function (req, res) {
   res.send("Sign up successful");
